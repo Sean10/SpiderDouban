8009ce93815d3fe7213ce1bf27538a37866fed64
sean10 <sean10reborn@gmail.com>
2017-12-17 19:10:42 +0800

    add parse and test

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 
 
 # 参考资料
-1. Python BloomFilter(https://axiak.github.io/pybloomfiltermmap/ref.html)
-2. rq(http://python-rq.org/docs/)
-3. lanbing510/DouBanSpider(https://github.com/lanbing510/DouBanSpider)
+1. [Python BloomFilter](https://axiak.github.io/pybloomfiltermmap/ref.html)
+2. [rq](http://python-rq.org/docs/)
+3. [lanbing510/DouBanSpider](https://github.com/lanbing510/DouBanSpider)
--- worker.py
+++ worker.py
@@ -1,5 +1,6 @@
 from redis import Redis,ConnectionPool
 from rq import Worker, Queue, Connection
+import time
 
 listen = ['high', 'default', 'low']
 
@@ -7,6 +8,9 @@ pool = ConnectionPool(db=0, host='localhost', port=6379)
 redis_conn = Redis(connection_pool=pool)
 
 if __name__ == '__main__':
+    start = time.time()
     with Connection(redis_conn):
         worker = Worker(map(Queue, listen))
         worker.work()
+        stop = time.time()
+        print(stop-start)
